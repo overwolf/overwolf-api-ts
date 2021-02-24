7e5f7ce4a3ad218c34188d15f38cb38b958c44e1
eransharv <eransharv@gmail.com>
2021-02-25 01:55:44 +0200

    update

--- dist/ow-hotkeys.d.ts
+++
@@ -1,6 +0,0 @@
-/// <reference types="@overwolf/types" />
-export declare class OWHotkeys {
-    private constructor();
-    static getHotkeyText(hotkeyId: string, gameId?: number): Promise<string>;
-    static onHotkeyDown(hotkeyId: string, action: (hotkeyResult: overwolf.settings.hotkeys.OnPressedEvent) => void): void;
-}
--- dist/ow-listener.d.ts
+++
@@ -1,8 +0,0 @@
-export interface OWListenerDelegate {
-}
-export declare abstract class OWListener<T extends OWListenerDelegate> {
-    protected _delegate: T;
-    constructor(delegate: T);
-    start(): void;
-    abstract stop(): void;
-}

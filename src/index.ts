85f1f44bc6c28a52409c61f50e37580ac24dc299
Anton Kulakov <anton.kulakov@overwolf.com>
2021-02-24 14:17:19 +0300

    set up typescript

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+export * from './ow-game-listener'
+export * from './ow-games-events'
+export * from './ow-games'
+export * from './ow-hotkeys'
+export * from './ow-listener'
+export * from './ow-window'
